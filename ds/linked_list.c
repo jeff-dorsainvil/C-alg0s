fb0c17c8a695aa1b5321fefeb71d3f90b3caf7f1
Jeff Dorsainvil <joedor7@gmail.com>
2022-11-20 22:35:45 -0500

    Started implementing DS

--- ds/linked_list.c
+++ ds/linked_list.c
@@ -1,6 +1,16 @@
+/**
+ * @file linked_list.c
+ * @author Jeff Dorsainvil
+ */
+
 #include <stdio.h>
 
+typedef struct {
+    char data[100];
+    struct Node *next;
+} Node;
+
 int main (){
-    
+
     return 0;
 }
